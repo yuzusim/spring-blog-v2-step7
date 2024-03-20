ed1bf1f46f5ba765866a3e347caeadbe5cf96a32
getinthere <getinthere@naver.com>
2024-03-20 10:40:45 +0900

    restapi 전환 준비

---
+++ src/main/java/shop/mtcoding/blog/board/BoardService.java
@@ -0,0 +1,95 @@
+package shop.mtcoding.blog.board;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shop.mtcoding.blog._core.errors.exception.Exception403;
+import shop.mtcoding.blog._core.errors.exception.Exception404;
+import shop.mtcoding.blog.reply.Reply;
+import shop.mtcoding.blog.reply.ReplyJPARepository;
+import shop.mtcoding.blog.user.User;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+
+    private final BoardJPARepository boardJPARepository;
+    private final ReplyJPARepository replyJPARepository;
+
+    public Board 글조회(int boardId){
+        Board board = boardJPARepository.findById(boardId)
+                .orElseThrow(() -> new Exception404("게시글을 찾을 수 없습니다"));
+        return board;
+    }
+
+    @Transactional
+    public void 글수정(int boardId, int sessionUserId, BoardRequest.UpdateDTO reqDTO){
+        // 1. 조회 및 예외처리
+        Board board = boardJPARepository.findById(boardId)
+                .orElseThrow(() -> new Exception404("게시글을 찾을 수 없습니다"));
+
+        // 2. 권한 처리
+        if(sessionUserId != board.getUser().getId()){
+            throw new Exception403("게시글을 수정할 권한이 없습니다");
+        }
+
+        // 3. 글수정
+        board.setTitle(reqDTO.getTitle());
+        board.setContent(reqDTO.getContent());
+    } // 더티체킹
+
+    @Transactional
+    public void 글쓰기(BoardRequest.SaveDTO reqDTO, User sessionUser){
+        boardJPARepository.save(reqDTO.toEntity(sessionUser));
+    }
+
+    @Transactional
+    public void 글삭제(int boardId, int sessionUserId) {
+        Board board = boardJPARepository.findById(boardId)
+                .orElseThrow(() -> new Exception404("게시글을 찾을 수 없습니다"));
+
+        if(sessionUserId != board.getUser().getId()){
+            throw new Exception403("게시글을 삭제할 권한이 없습니다");
+        }
+
+        boardJPARepository.deleteById(boardId);
+    }
+
+    public List<Board> 글목록조회() {
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        return boardJPARepository.findAll(sort);
+    }
+
+    // board, isOwner
+    public Board 글상세보기(int boardId, User sessionUser) {
+        Board board = boardJPARepository.findByIdJoinUser(boardId)
+                .orElseThrow(() -> new Exception404("게시글을 찾을 수 없습니다"));
+
+        boolean isBoardOwner = false;
+        if(sessionUser != null){
+            if(sessionUser.getId() == board.getUser().getId()){
+                isBoardOwner = true;
+            }
+        }
+
+        board.setBoardOwner(isBoardOwner);
+
+        board.getReplies().forEach(reply -> {
+            boolean isReplyOwner = false;
+
+            if(sessionUser != null){
+                if(reply.getUser().getId() == sessionUser.getId()){
+                    isReplyOwner = true;
+                }
+            }
+            reply.setReplyOwner(isReplyOwner);
+        });
+
+        
+
+        return board;
+    }
+}
---
+++ src/main/java/shop/mtcoding/blog/reply/ReplyController.java
@@ -0,0 +1,29 @@
+package shop.mtcoding.blog.reply;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import shop.mtcoding.blog.user.User;
+
+@RequiredArgsConstructor
+@Controller
+public class ReplyController {
+    private final ReplyService replyService;
+    private final HttpSession session;
+
+    @PostMapping("/board/{boardId}/reply/{replyId}/delete")
+    public String delete(@PathVariable Integer boardId, @PathVariable Integer replyId){
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        replyService.댓글삭제(replyId, sessionUser.getId());
+        return "redirect:/board/"+boardId;
+    }
+
+    @PostMapping("/reply/save")
+    public String save(ReplyRequest.SaveDTO reqDTO){
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        replyService.댓글쓰기(reqDTO, sessionUser);
+        return "redirect:/board/"+reqDTO.getBoardId();
+    }
+}
